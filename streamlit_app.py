736bd0c0fb9f33aa1aa61277c05530c2aa2cd498
Swati5254 <133072617+Swati5254@users.noreply.github.com>
2023-07-13 17:56:18 +0530

    Update streamlit_app.py
    added the text as menu
    added the text as menu
    added the text as menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 & blueberry oatmeal')
+streamlit.text('kale,spinach & Rocket')
+streamlit.text('Hard Boiled Free-Range Egg')
